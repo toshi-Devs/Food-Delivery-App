e0946c8e1136f087f153eaad884f762bbea72d90
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-05 19:23:47 +0200

    Notifs set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Notifications from "@/components/Notifications";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <Notifications />
+        <Navbar />
+        
+        {children}
+
+        <Footer />
+        
+      </body>
     </html>
   );
 }
---
+++ src/components/Notifications.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <div className='bg-red-500 h-12 text-white flex items-center text-center justify-center px-4 text-sm md:text-base'>
+        free shipping on orders over $50. Order food now!
+
+    </div>
+  )
+}
+
+export default Notifications
