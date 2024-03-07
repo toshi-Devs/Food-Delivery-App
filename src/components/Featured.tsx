7dd9f5f7b27ff2c820be488a046b4ddbc267040e
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-07 11:05:02 +0200

    featured products set

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,39 @@
+import { featuredProducts } from '@/data'
+import Image from 'next/image'
 import React from 'react'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='w-screen overflow-x-scroll text-red-500'>
+        <div className='w-max flex'>
+
+        {featuredProducts.map((item) => (
+
+            <div key={item.id}
+             className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[32vw] xl:h-[90vh]'>
+
+            { item.img && (   
+                 <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+
+                    <Image 
+                    src={item.img} 
+                    alt='img' fill 
+                    className='object-contain' />
+
+                </div> )}
+                    
+                <div className='flex-1 flex flex-col gap-4 items-center text-center justify-center'>
+
+                    <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl '>{item.title}</h1>
+                    <p className='p-4 2xl:p-8'>{item.desc}</p>
+                    <span className='text-xl font-bold '>€{item.price}</span>
+                    <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+
+                </div>
+            </div>
+        ))}
+        </div>
+    </div>
   )
 }
 
