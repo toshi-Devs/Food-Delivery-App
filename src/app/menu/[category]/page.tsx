05530c22ed2939688153331afe7539bfdf1235c6
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-07 17:00:45 +0200

    productList done

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,32 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>CategoryPage</div>
+    <div className='flex flex-wrap text-red-500'>
+
+      {pizzas.map((item)=> (
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 md:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-100'
+              href={`/product/${item.id}`}
+              key={item.id}>
+
+              {item.img && (
+                <div className='relative h-[80%]'>
+                  <Image src={item.img} alt='img' fill className='object-contain'/>
+                </div>
+              )}
+
+              <div className='flex items-center justify-between font-bold '>
+                <h1 className='text-xl uppercase p-2'>{item.title}</h1>
+                <h2 className='group-hover:hidden text-xl'>€{item.price}</h2>
+                <button className='hidden group-hover:block uppercase text-white bg-red-500 p-2 rounded-md'>Add to Cart</button>
+              </div>
+        </Link>
+      ))}
+      
+    </div>
   )
 }
 
